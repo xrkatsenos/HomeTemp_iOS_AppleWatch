d7ecdecf0d22b69a8c7555dd2ca3af0c5ba3b29d
Christos Katsenos <christos.katsenos@ericsson.com>
2020-02-08 18:33:48 +0200

    Initial Commit

---
+++ HomeTemp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeTemp
+//
+//  Created by Katsenos Christos on 8/2/20.
+//  Copyright © 2020 Katsenos Christos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
